24ad078c8387c9db335caaf777b911de99eed507
CrisUltraHD <cristiancaride@gmail.com>
2020-11-24 18:50:10 +0100

    Segona Habitacio Feta Sense Bugs

--- Assets/Scripts/SegonaHabitacio/SegonaHabitació.cs
+++ Assets/Scripts/SegonaHabitacio/SegonaHabitació.cs
@@ -1,29 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SegonaHabitació : MonoBehaviour
 {
 
     public GameObject panelLlaves;
+    public GameObject imagenLlave;
+
+    //EL NUMERO DE LA CLAU QUE USU INSEREIX
+    public Text numClau;
 
     // Start is called before the first frame update
     void Start()
     {
         panelLlaves.SetActive(false);
+        imagenLlave.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "player") 
+        if (other.gameObject.tag == "CAJONPARTE2") 
         {
             panelLlaves.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "CAJONPARTE2")
+        {
+            panelLlaves.SetActive(false);
+        }
+    }
+
+    //BOTÓ VALIDAR
+    //CORRECTA ES 1
+    public void Validar()
+    {
+        if (numClau.text.Equals("1"))
+        {
+            imagenLlave.SetActive(true);
+        }
+
+        else
+        {
+            numClau.text = "";
+        }
+
     }
 }
