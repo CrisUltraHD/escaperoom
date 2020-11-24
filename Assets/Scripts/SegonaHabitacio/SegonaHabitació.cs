6e47fa9d274c2be7abd4fc9451d0e83be070018e
CrisUltraHD <cristiancaride@gmail.com>
2020-11-24 17:56:59 +0100

    Afegit Habitació 2

---
+++ Assets/Scripts/SegonaHabitacio/SegonaHabitació.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegonaHabitació : MonoBehaviour
+{
+
+    public GameObject panelLlaves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelLlaves.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "player") 
+        {
+            panelLlaves.SetActive(true);
+        }
+    }
+}
